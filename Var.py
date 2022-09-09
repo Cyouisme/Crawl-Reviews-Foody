e92b950f6a84ff7e4954fad62e0f9248fafa58d5
cyouisme <baochunga1@gmail.com>
2022-09-09 08:20:50 +0700

    All code crawl data from foody

---
+++ Var.py
@@ -0,0 +1,10 @@
+EMAIL = "hbcqb2k@gmail.com"
+NUM_ITEM = 150
+NUM_FIRST_PAGE = 37
+PAGE_START = 1
+URL = "https://www.foody.vn/ho-chi-minh/travel/dia-diem?q="
+HEADER = [' ','Review', 'giai_tri']
+PATH_SAVE = 'data_crawled/hcm/data_final_pro_2_crawl_hcm_giai_tri.csv'
+PATH_BUGLIST = 'data_crawled/hcm/buglist_hcm_giai_tri.csv'
+LIST_TEXT_CHECK = ["Khách sạn"]
+# PATH_SAVE = 'data_crawled/data_final_pro_2_crawl_jolibie.csv'
---
+++ data_crawled/fill_zeros.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+file = pd.read_csv("data_final_pro_2_crawl_nha_hang_500.csv")
+
+f = file.copy()
+
+for i in f.iloc[0:109, 2:8]:
+	f[i][0:109] = f[i][0:109].fillna(0)
+
+# print(f.head())
+
+
+#Define csv to utf8 for vietnamese
+f.to_csv("./danang/data_final_pro_2_crawl_nha_hang_500.csv", encoding='utf-8-sig')
